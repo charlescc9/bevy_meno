686f67f3184c2a1f04020bcaeee8a70fe894fcb1
Charles Costello <charles@meno.local>
2024-07-19 20:18:19 -0600

    Refined grid

--- src/main.rs
+++ src/main.rs
@@ -18,30 +18,51 @@ fn setup(
 ) {
     commands.spawn(Camera2dBundle::default());
 
-    for i in -20..=20 {
+    let cell_size = 16.0;
+    let border_size = 2.0;
+    let num_rows = 20;
+
+    for i in -num_rows..=num_rows {
         commands.spawn(MaterialMesh2dBundle {
-            mesh: Mesh2dHandle(meshes.add(Rectangle::new(5.0, 2000.0))),
+            mesh: Mesh2dHandle(meshes.add(Rectangle::new(
+                border_size,
+                2.0 * cell_size * num_rows as f32,
+            ))),
             material: materials.add(Color::srgb(0.5, 0.5, 0.5)),
-            transform: Transform::from_xyz(i as f32 * 50.0, 0.0, 0.0),
+            transform: Transform::from_xyz(i as f32 * cell_size, 0.0, 0.0),
             ..default()
         });
     }
 
-    for i in -20..=20 {
+    for i in -num_rows..=num_rows {
         commands.spawn(MaterialMesh2dBundle {
-            mesh: Mesh2dHandle(meshes.add(Rectangle::new(2000.0, 5.0))),
+            mesh: Mesh2dHandle(meshes.add(Rectangle::new(
+                2.0 * cell_size * num_rows as f32,
+                border_size,
+            ))),
             material: materials.add(Color::srgb(0.5, 0.5, 0.5)),
-            transform: Transform::from_xyz(0.0, i as f32 * 50.0, 0.0),
+            transform: Transform::from_xyz(0.0, i as f32 * cell_size, 0.0),
             ..default()
         });
     }
 
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: Mesh2dHandle(meshes.add(Rectangle::new(45.0, 45.0))),
-        material: materials.add(Color::srgb(1.0, 1.0, 1.0)),
-        transform: Transform::from_xyz(25.0, 25.0, 0.0),
-        ..default()
-    });
+    for i in -num_rows..num_rows {
+        for j in -num_rows..num_rows {
+            commands.spawn(MaterialMesh2dBundle {
+                mesh: Mesh2dHandle(meshes.add(Rectangle::new(
+                    cell_size - border_size,
+                    cell_size - border_size,
+                ))),
+                material: materials.add(Color::srgb(1.0, 1.0, 1.0)),
+                transform: Transform::from_xyz(
+                    i as f32 * cell_size + (cell_size / 2.0),
+                    j as f32 * cell_size + (cell_size / 2.0),
+                    0.0,
+                ),
+                ..default()
+            });
+        }
+    }
 }
 
 fn exit_system(mut exit: EventWriter<AppExit>, keyboard: Res<ButtonInput<KeyCode>>) {
