4e35905b2dee418a09c7abe8b71e73add410492e
Charles Costello <charles@meno.local>
2024-07-29 19:50:04 -0600

    Added game state board

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,34 @@ const WINDOW_WIDTH: f32 = 1600.0;
 const WINDOW_HEIGHT: f32 = 1048.0;
 const CELL_SIZE: f32 = 16.0;
 const BORDER_SIZE: f32 = 2.0;
-const NUM_ROWS: i32 = 64;
-const GRID_COLOR: Color = Color::srgb(0.5, 0.5, 0.5);
+const BOARD_SIZE: i32 = 64;
+const BOARD_COLOR: Color = Color::srgb(0.5, 0.5, 0.5);
 const ALIVE_CELL_COLOR: Color = Color::WHITE;
 const DEAD_CELL_COLOR: Color = Color::BLACK;
 
 #[derive(Resource)]
 struct GameTimer(Timer);
 
+#[derive(Resource)]
+struct GameState {
+    board: [[CellState; BOARD_SIZE as usize]; BOARD_SIZE as usize]
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        GameState {
+            board: [[CellState::Dead; BOARD_SIZE as usize]; BOARD_SIZE as usize]
+        }
+    }
+}
+
 #[derive(Component)]
+struct CellPosition {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component, Clone, Copy, Debug)]
 enum CellState {
     Alive,
     Dead,
@@ -27,6 +46,7 @@ enum CellState {
 
 #[derive(Bundle)]
 struct CellBundle {
+    cell_position: CellPosition,
     state: CellState,
     material_mesh_bundle: MaterialMesh2dBundle<ColorMaterial>,
 }
@@ -40,22 +60,22 @@ fn setup(
     commands.spawn(Camera2dBundle::default());
 
     // Spawn grid
-    for i in -(NUM_ROWS / 2)..=(NUM_ROWS / 2) {
+    for i in -(BOARD_SIZE / 2)..=(BOARD_SIZE / 2) {
         commands.spawn(MaterialMesh2dBundle {
             mesh: Mesh2dHandle(
-                meshes.add(Rectangle::new(BORDER_SIZE, CELL_SIZE * NUM_ROWS as f32)),
+                meshes.add(Rectangle::new(BORDER_SIZE, CELL_SIZE * BOARD_SIZE as f32)),
             ),
-            material: materials.add(GRID_COLOR),
+            material: materials.add(BOARD_COLOR),
             transform: Transform::from_xyz(i as f32 * CELL_SIZE, 0.0, 0.0),
             ..default()
         });
     }
-    for i in -(NUM_ROWS / 2)..=(NUM_ROWS / 2) {
+    for i in -(BOARD_SIZE / 2)..=(BOARD_SIZE / 2) {
         commands.spawn(MaterialMesh2dBundle {
             mesh: Mesh2dHandle(
-                meshes.add(Rectangle::new(CELL_SIZE * NUM_ROWS as f32, BORDER_SIZE)),
+                meshes.add(Rectangle::new(CELL_SIZE * BOARD_SIZE as f32, BORDER_SIZE)),
             ),
-            material: materials.add(GRID_COLOR),
+            material: materials.add(BOARD_COLOR),
             transform: Transform::from_xyz(0.0, i as f32 * CELL_SIZE, 0.0),
             ..default()
         });
@@ -66,6 +86,7 @@ fn spawn_cells(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    mut game_state: ResMut<GameState>
 ) {
     let cell_mesh_handle = Mesh2dHandle(meshes.add(Rectangle::new(
         CELL_SIZE - BORDER_SIZE,
@@ -76,22 +97,29 @@ fn spawn_cells(
     let mut rng = rand::thread_rng();
 
     for _ in 0..NUM_CELLS {
-        let mut i = rng.gen_range(-(NUM_ROWS / 2)..(NUM_ROWS / 2));
-        let mut j = rng.gen_range(-(NUM_ROWS / 2)..(NUM_ROWS / 2));    
+        
+        // Get new board index
+        let mut i = rng.gen_range(-(BOARD_SIZE / 2)..(BOARD_SIZE / 2));
+        let mut j = rng.gen_range(-(BOARD_SIZE / 2)..(BOARD_SIZE / 2));    
         loop {
             if !occupied_cells.contains(&(i, j)) {
                 occupied_cells.insert((i, j));
                 break;
             } else {
-                i = rng.gen_range(-(NUM_ROWS / 2)..(NUM_ROWS / 2));
-                j = rng.gen_range(-(NUM_ROWS / 2)..(NUM_ROWS / 2));    
+                i = rng.gen_range(-(BOARD_SIZE / 2)..(BOARD_SIZE / 2));
+                j = rng.gen_range(-(BOARD_SIZE / 2)..(BOARD_SIZE / 2));    
             }
         }
+        let i_absolute = i + (BOARD_SIZE / 2);
+        let j_absolute = j + (BOARD_SIZE / 2);
+
+        // Spawn cell
         commands.spawn(CellBundle {
+            cell_position: CellPosition { x: i_absolute, y: j_absolute },
             state: CellState::Dead,
             material_mesh_bundle: MaterialMesh2dBundle {
                 mesh: cell_mesh_handle.clone(),
-                material: materials.add(DEAD_CELL_COLOR),
+                material: materials.add(ALIVE_CELL_COLOR),
                 transform: Transform::from_xyz(
                     i as f32 * CELL_SIZE + (CELL_SIZE / 2.0),
                     j as f32 * CELL_SIZE + (CELL_SIZE / 2.0),
@@ -100,6 +128,9 @@ fn spawn_cells(
                 ..default()
             },
         });
+
+        // Update board
+        game_state.board[i_absolute as usize][j_absolute as usize] = CellState::Alive;
     }
 }
 
@@ -108,6 +139,7 @@ fn update(
     mut timer: ResMut<GameTimer>,
     mut query: Query<(&mut CellState, &Handle<ColorMaterial>)>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    game_state: ResMut<GameState>
 ) {
     if timer.0.tick(time.delta()).just_finished() {
         for (mut state, handle) in &mut query {
@@ -137,7 +169,9 @@ fn exit_system(mut exit: EventWriter<AppExit>, keyboard: Res<ButtonInput<KeyCode
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(DEAD_CELL_COLOR))
         .insert_resource(GameTimer(Timer::from_seconds(1.0, TimerMode::Repeating)))
+        .insert_resource(GameState::default())
         .add_plugins((DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Meno".into(),
